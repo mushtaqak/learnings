db9c6c7268ddb064560233b7af6929a1e22a3e17
Mushtaq Ali <mushtaak@gmail.com>
2021-06-24 17:17:39 +0500

    fix author entity

--- tutorials/js/nestjs/bookshelf/src/author/entities/author.entity.ts
+++ tutorials/js/nestjs/bookshelf/src/author/entities/author.entity.ts
@@ -14,12 +14,16 @@ export class Author {
   name: string;
 
   @Field(() => [Book])
-  // FIXME: Getting books in authors query
-  // cascade: true is needed in one-to-many side to save related book in DB.
+  // cascade: true
+  // - allows to apply all DML on related relation (insert / update / delete)
+  // - is needed in one-to-many side to save related book in DB.
   // lazy: true
   // - fixes "Cannot return null for non-nullable field Book.author" when querying books query.
   // - It lazily fetches data.
-  // - it seems it causes issue when saving data in DB.
-  @OneToMany(() => Book, (book) => book.author, { cascade: true })
+  // - it seems it causes issue when saving data in DB, so it can not be set with cascade: true
+  // eager: true
+  // - Eager relations are always loaded automatically when relation's owner entity is loaded.
+  // - we can only make one relation eager just like cascade.
+  @OneToMany(() => Book, (book) => book.author, { cascade: true, eager: true })
   books?: Book[];
 }
