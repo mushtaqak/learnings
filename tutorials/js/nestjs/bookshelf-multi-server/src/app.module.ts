d6cb1bc476ca47d2edcdc4c9c5b6c3d845db3cd7
Mushtaq Ali <mushtaak@gmail.com>
2021-06-29 21:35:40 +0500

    tutorial(nestjs-graphql-bookshelf-multi-server): updates

--- tutorials/js/nestjs/bookshelf-multi-server/src/app.module.ts
+++ tutorials/js/nestjs/bookshelf-multi-server/src/app.module.ts
@@ -4,35 +4,36 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RestModule } from './rest/rest.module';
+import { AuthorModule } from './author/author.module';
 
-@Module({
-  imports: [
-    // TODO: use database configs from env
-    // TypeOrmModule.forRoot(DatabaseConfig() as ConnectionOptions),
-    // for now directly pass typeorm settings to nestjs
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'password',
-      database: 'bookshelf',
-      // entities: [Author, Book],
-      synchronize: true,
-    }),
-    GraphQLModule.forRoot({
-      autoSchemaFile: 'schema.gql',
-    }),
-    // AuthorModule, BookModule,
-    RestModule,
-  ],
-  controllers: [AppController],
-  providers: [AppService],
-})
+@Module({})
 export class AppModule {
   static register(options): DynamicModule {
     return {
-      module: RestModule,
+      module: AppModule,
+      imports: [
+        // TODO: use database configs from env
+        // TypeOrmModule.forRoot(DatabaseConfig() as ConnectionOptions),
+        // for now directly pass typeorm settings to nestjs
+        TypeOrmModule.forRoot({
+          type: 'postgres',
+          host: 'localhost',
+          port: 5432,
+          username: 'postgres',
+          password: 'password',
+          database: 'bookshelf',
+          // entities: [Author, Book],
+          synchronize: true,
+        }),
+        GraphQLModule.forRoot({
+          autoSchemaFile: 'schema.gql',
+        }),
+        // AuthorModule, BookModule,
+        RestModule,
+        AuthorModule,
+      ],
+      controllers: [AppController],
+      providers: [AppService],
     };
   }
 }
