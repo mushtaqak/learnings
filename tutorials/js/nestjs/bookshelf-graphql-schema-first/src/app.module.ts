845a616faa854271cc87bba806fa3fb20a2f03af
Mushtaq Ali <mushtaak@gmail.com>
2021-06-29 13:17:35 +0500

    tutorial(nestjs-graphql-bookshelf-schema-first): Add authors

--- tutorials/js/nestjs/bookshelf-graphql-schema-first/src/app.module.ts
+++ tutorials/js/nestjs/bookshelf-graphql-schema-first/src/app.module.ts
@@ -1,9 +1,33 @@
+import { join } from 'path';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthorModule } from './author/author.module';
 
 @Module({
-  imports: [],
+  imports: [
+    // TODO: use database configs from env
+    // TypeOrmModule.forRoot(DatabaseConfig() as ConnectionOptions),
+    // for now directly pass typeorm settings to nestjs
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'password',
+      database: 'bookshelf',
+      synchronize: true,
+    }),
+    GraphQLModule.forRoot({
+      typePaths: ['./**/*.graphql'],
+      // definitions: {
+      //   path: join(process.cwd(), 'src/graphql.ts'),
+      // },
+    }),
+    AuthorModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
