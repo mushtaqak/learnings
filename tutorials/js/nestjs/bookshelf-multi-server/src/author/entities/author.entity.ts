c2810aca9fade8f03c2d0f5fa274717233be125e
Mushtaq Ali <mushtaak@gmail.com>
2021-06-30 17:04:34 +0500

    tutorial(nestjs-graphql-bookshelf-multi-server): add typeorm to authors entity

--- tutorials/js/nestjs/bookshelf-multi-server/src/author/entities/author.entity.ts
+++ tutorials/js/nestjs/bookshelf-multi-server/src/author/entities/author.entity.ts
@@ -1,7 +1,10 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { Column, Entity } from 'typeorm';
 
 @ObjectType()
+@Entity()
 export class Author {
   @Field(() => Int, { description: 'Example field (placeholder)' })
+  @Column()
   exampleField: number;
 }
