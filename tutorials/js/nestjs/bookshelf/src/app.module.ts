289c758aada03a2763f8f18d7e3d13d0bcd864b8
Mushtaq Ali <mushtaak@gmail.com>
2021-07-01 11:54:35 +0500

    tutorial(nestjs-graphql-bookshelf): add autoLoadEnitites

--- tutorials/js/nestjs/bookshelf/src/app.module.ts
+++ tutorials/js/nestjs/bookshelf/src/app.module.ts
@@ -4,13 +4,24 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthorModule } from './author';
-import { BookModule } from './book';
+import { Author, AuthorModule } from './author';
+import { Book, BookModule } from './book';
 import DatabaseConfig from './config/database.config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(DatabaseConfig() as ConnectionOptions),
+    // TypeOrmModule.forRoot(DatabaseConfig() as ConnectionOptions),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'password',
+      database: 'bookshelf',
+      // entities: [Author, Book],
+      autoLoadEntities: true, // this auto loads entities so we do not need entities attr
+      synchronize: true,
+    }),
     GraphQLModule.forRoot({
       autoSchemaFile: 'schema.gql',
     }),
