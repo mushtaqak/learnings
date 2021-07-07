b6601fda7bbe801369477502e768b5864af254d2
Mushtaq Ali <mushtaak@gmail.com>
2021-07-07 14:06:46 +0500

    tutorial(nestjs-graphql-bookshelf): add file upload functionality

--- tutorials/js/nestjs/bookshelf/src/script-runner/script-runner.controller.ts
+++ tutorials/js/nestjs/bookshelf/src/script-runner/script-runner.controller.ts
@@ -1,7 +1,30 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseInterceptors,
+  UploadedFile,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
 import { ScriptRunnerService } from './script-runner.service';
 import { CreateScriptRunnerDto } from './dto/create-script-runner.dto';
 import { UpdateScriptRunnerDto } from './dto/update-script-runner.dto';
+
+// const SCRIPTS_UPLOAD_DIR = 'src/script-runner/scripts';
+const SCRIPTS_UPLOAD_DIR = './scripts';
+
+// file filter for scripts
+export const scriptFileFilter = (req, file, callback) => {
+  if (!file.originalname.match(/\.(ts|js)$/)) {
+    return callback(new Error('Only javascript files are allowed!'), false);
+  }
+  callback(null, true);
+};
 
 @Controller('script-runner')
 export class ScriptRunnerController {
@@ -10,6 +33,24 @@ export class ScriptRunnerController {
   @Post()
   create(@Body() createScriptRunnerDto: CreateScriptRunnerDto) {
     return this.scriptRunnerService.create(createScriptRunnerDto);
+  }
+
+  @Post('upload')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: SCRIPTS_UPLOAD_DIR,
+        filename: (eq, file, callback) => callback(null, file.originalname),
+      }),
+      fileFilter: scriptFileFilter,
+    }),
+  )
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    // console.log({ file });
+    const response = {
+      filename: file.originalname,
+    };
+    return response;
   }
 
   @Get()
@@ -23,7 +64,10 @@ export class ScriptRunnerController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateScriptRunnerDto: UpdateScriptRunnerDto) {
+  update(
+    @Param('id') id: string,
+    @Body() updateScriptRunnerDto: UpdateScriptRunnerDto,
+  ) {
     return this.scriptRunnerService.update(id, updateScriptRunnerDto);
   }
 
