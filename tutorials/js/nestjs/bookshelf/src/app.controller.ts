dab2ba1e23e4a59080f607de530b36fddb9c9c5a
Mushtaq Ali <mushtaak@gmail.com>
2021-07-14 13:50:16 +0500

    tutorial(nestjs-graphql-bookshelf):  add templating

--- tutorials/js/nestjs/bookshelf/src/app.controller.ts
+++ tutorials/js/nestjs/bookshelf/src/app.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('hello')
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get()
+  @Render('index')
+  root() {
+    return { message: 'Hello world from template!' };
   }
 }
--- tutorials/js/nestjs/bookshelf/src/main.ts
+++ tutorials/js/nestjs/bookshelf/src/main.ts
@@ -1,13 +1,15 @@
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import * as compression from 'compression';
 import * as cookieParser from 'cookie-parser';
 import * as session from 'express-session';
+import { join } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     // logger: ['error', 'warn'],
   });
   // Compression can greatly decrease the size of the response body, thereby increasing the speed of a web app
@@ -40,7 +42,12 @@ async function bootstrap() {
 
   // auto-validation --- this will also add validation response messages
   app.useGlobalPipes(new ValidationPipe());
-  
+
+  // MVC settings
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('hbs') // (Handlebars) engine --- we can set any templating engine like ejs
+
   // start app
   await app.listen(port);
 }
